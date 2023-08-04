33bfb51c436623dc0f9fd7ff9ed7527f5ce59430
xtashi <tcharlles.us@gmail.com>
2023-08-04 15:35:49 -0300

    add men role conditions

--- utils/getCondition.js
+++ utils/getCondition.js
@@ -3,12 +3,13 @@ const {
   genericCommons,
 } = require("../utils/responses/generics");
 const { womenConditionals, womenCommons } = require("./responses/w");
+const { menConditionals, menCommons } = require("./responses/h");
 const { adConditionals, adCommons } = require("./responses/s");
 
 const dotenv = require("dotenv");
 dotenv.config();
 
-const { CARGO_MULHER, CARGO_ADM } = process.env;
+const { CARGO_MULHER, CARGO_HOMEM, CARGO_ADM } = process.env;
 
 const getCondition = (message) => {
   if (message.member.roles.cache.some((role) => role.name === CARGO_ADM))
@@ -18,6 +19,11 @@ const getCondition = (message) => {
       commons: womenCommons,
       conditionals: womenConditionals,
     };
+  if (message.member.roles.cache.some((role) => role.name === CARGO_HOMEM))
+    return {
+      commons: menCommons,
+      conditionals: menConditionals
+    }
 
   return { commons: genericCommons, conditionals: genericConditionals };
 };
